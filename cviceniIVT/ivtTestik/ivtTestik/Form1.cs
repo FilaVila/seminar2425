0e55ed4a07443c2d77aa330c3cf0a28fe4d88190
FilaVila <144894437+FilaVila@users.noreply.github.com>
2024-11-19 00:07:28 +0100

    cviceniIVT

---
+++ cviceniIVT/ivtTestik/ivtTestik/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ivtTestik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static int LoadNumber(string input) // tato statická funkce načte input od uživatele a zkusí jestli je to integer
+        {
+            int number = 0;
+            bool isInvalid = false; //boolean proměnná kontrolující while cyklus
+            while (isInvalid == false)
+            {
+                try
+                {
+                    number = int.Parse(input);
+                    isInvalid = true;
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Hele to asi nebude integer zkus to znova");
+                }
+            }
+            return number;
+        }
+        static float RoundIt(float number) //statická funkce načte float zaokrouhlí ho a vrátí
+        {
+            float fNumber = (float)Math.Round(number, 1);
+            return fNumber; //fNumber = finished number
+        }
+
+        private async Task Write(string message) //async funkce umožňuje provádět operaci aniž by to "zastavilo zbylý chod aplikace"
+        {                                       //šikovné pro práci s časem, tady je odkaz na vysvětlení:https://learn.microsoft.com/en-us/dotnet/csharp/asynchronous-programming/
+            string eMessage =""; // empty message
+            foreach (var item in message) 
+            {
+                eMessage = eMessage + item.ToString();
+                textBox2.Text=(eMessage);
+                await Task.Delay(30);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = LoadNumber(textBox1.Text);
+            float z = RoundIt(3.0f * a * 7); //3 kg zrní na 1 krávu * počet krav * 7 dní v týdnu
+            float s = RoundIt(0.4f * a * 7); // za 0.4 je f, abychom to definovali jako float (nemůžeme z integeru a doublu udělat jen tak float)
+            float v = RoundIt(10f * a * 7); // v jako voda :D
+            Write("Na týden bude potřeba " + z + " kg zrní a " + s + " balíků sena " + v + " l vody.");
+            //textBox2.Text = ("Na týden bude potřeba "+ z +" kg zrní a " + s + " balíků sena "+v +" l vody."); //alternativní možnost pro lidi z Omský
+            MessageBox.Show("Kolik má kráva žaludků?");
+        }
+    }
+}
---
+++ cviceniIVT/krmivoConsole/krmivoConsole/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms; // přidal jsem knihovnu pomocí záložky references, abych mohl kingovat s messageboxem
+using System.Threading.Tasks;
+
+namespace krmivoConsole
+{
+    internal class Program
+    {
+        static int LoadNumber() //"funkce" načte input od uživatele a zkusí jestli je to integer, a bude to zkouset dokud se uzivatel netrefí
+        {
+            int number = 0;
+            bool isValid = false; // //boolean proměnná kontrolující while cyklus
+            while (isValid == false)
+            {
+                try
+                {
+                    number = int.Parse(Console.ReadLine());
+                    isValid = true;
+                }
+                catch (FormatException)
+                {
+                    Write("Hele to asi nebude integer zkus to znova");
+                }
+            }
+            return number;
+        }
+        static void Write(string m) //string m = string message, voidová funkce nevrací žádnou hodnotu
+        {            
+            foreach (char l in m)
+            {
+                Console.Write(l);
+                Thread.Sleep(25);
+            }
+            Console.WriteLine("\n");
+        }
+        static float RoundIt(float number) //statická funkce načte float zaokrouhlí ho a vrátí
+        {
+            float fNumber = (float)Math.Round(number, 1);
+            return fNumber; //fNumber = finished number
+        }
+        static void Main(string[] args)
+        {
+            Write("Zdravím farmáři zadej velikost tvého stáda.");
+            int a = LoadNumber();
+            float z = RoundIt(3.0f * a * 7); //3 kg zrní na 1 krávu * počet krav * 7 dní v týdnu
+            float s = RoundIt(0.4f * a * 7); // za 0.4 je f, abychom to definovali jako float (nemůžeme z integeru a doublu udělat jen tak float)
+            float v = RoundIt(10f * a * 7); // v jako voda :D
+            string o = "Na týden bude potřeba " + z + " kg zrní " + s + " balíků sena a " + v + " l vody.";
+            Console.ForegroundColor = ConsoleColor.Red;           
+            Write(o);
+            Console.ResetColor();
+            MessageBox.Show("Kolik má kráva žaludků?\nA jak se Filipovi podařilo zapnout MessageBox v konzolové aplikaci?");
+            Console.ReadKey();
+        }
+    }
+}
