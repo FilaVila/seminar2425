d5df0e9015bcb369f112b16dbb89d1f6a51be697
FilaVila <144894437+FilaVila@users.noreply.github.com>
2025-02-14 11:20:26 +0100

    uzmeneser

---
+++ grafy/grafy/Node.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grafy
+{
+    internal class Node
+    {
+        public int index;
+        public List<Node> neighbours;
+        public Node(int index)
+        {
+            this.index = index;
+            neighbours = new List<Node>();
+        }
+
+        public void AddNeighbours(List<Node> ToAdd) 
+        {
+            foreach (Node node in ToAdd) 
+            {
+                if (neighbours.Contains(node))
+                {
+                    Console.WriteLine("Soused "+ node.index + " is already in the list");
+                }
+                else
+                {
+                    neighbours.Add(node);
+                }
+            }
+            //neighbours.AddRange(newnighboursToAdd);
+        }
+        public void Printneighbours() 
+        {
+            Console.Write("Node "+index+" has neighbours ");
+            foreach (Node node in neighbours) 
+            {
+                Console.WriteLine(node.index + " ");
+            }
+        }
+        public Node TraverseNode(int indexToTravelrse) 
+        {
+            foreach(Node node in neighbours) 
+            {
+                if (node.index == indexToTravelrse)
+                {
+                    return node;
+                }
+            }
+            Console.WriteLine("sry tento bod s tím nesousedí");
+            return this; //vrátí index na kterém zrovna jsme
+        }
+    }
+}
---
+++ grafy/grafy/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grafy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Node node1 = new Node(1);
+            Node node2 = new Node(2);
+            Node node3 = new Node(3);
+            Node node4 = new Node(4);   
+            Node node5 = new Node(5);
+            Node node6 = new Node(6);
+            Node node7 = new Node(7);
+            node1.AddNeighbours(new List<Node> { node3, node7 });
+            node2.AddNeighbours(new List<Node> { node3, node4, node5 });
+            node3.AddNeighbours(new List<Node> {node1, node2, node6});
+            node4.AddNeighbours(new List<Node> { node7, node2, node5});
+            node5.AddNeighbours(new List<Node> {node2, node4, node6});
+            node6.AddNeighbours(new List<Node> {node4, node5, node7});
+            node7.AddNeighbours(new List<Node> {node1, node4, node6});
+            Node currentNode = node1;
+            while (true) 
+            {
+                currentNode.Printneighbours();
+                int indexToGo = int.Parse(Console.ReadLine());
+                currentNode = currentNode.TraverseNode(indexToGo);
+            }
+
+        }
+    }
+}
